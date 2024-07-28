5e895c3d45ebac41b6bc5e8a3522e9f164c0d374
QCanvas <QCanvas@noreply.codeberg.org>
2024-07-28 10:23:06 +1000

    Add meta-repo updater

--- update.sh
+++ update.sh
@@ -13,7 +13,6 @@ cd .work-dir/QCanvas || exit
 # Reset the repo, fetch tags and pull
 git reset HEAD --hard
 git fetch --tags
-git pull
 
 # Get the tag information
 latest_tag=$(git describe --tags "$(git rev-list --tags --max-count=1)")
@@ -27,5 +26,7 @@ poetry export --without-hashes -f requirements.txt -o ../requirements.txt
 
 # Move to root dir
 cd ../..
+
 # Re-generate the manifest
 req2flatpak --requirements-file .work-dir/requirements.txt --target-platforms 311-x86_64 311-aarch64 --outfile python3-qcanvas.json --requirements qcanvas=="${pypi_version_number}"
+pdm run utils/update-meta-repo.py .work-dir/QCanvas qcanvas-desktop-files.json
